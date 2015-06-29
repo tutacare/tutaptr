455f9dd8dd3c7b7e86881c5cdd83858eef2612b5
Irfan Mahfudz Guntur <tutacare@gmail.com>
2015-06-29 18:08:07 +0800

    add register member

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,6 +25,10 @@ Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::get('dashboard', 'DashboardController@index');
 Route::resource('dashboard/posts', 'PostController');
 
