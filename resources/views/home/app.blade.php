596257825fd4644a9b312a7acfe3caaf86e8e1eb
Irfan Mahfudz Guntur <tutacare@gmail.com>
2015-07-01 19:23:32 +0800

    fix template

--- resources/views/home/app.blade.php
+++ resources/views/home/app.blade.php
@@ -11,13 +11,6 @@
     <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
     <link rel="shortcut icon" href="http://tutahosting.net/wp-content/uploads/2015/01/tutaico.png" type="image/x-icon" />
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
     <nav class="navbar navbar-default">
@@ -29,24 +22,24 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="http://tutacare.github.io/tutapos/">TutaPTR</a>
+                <a class="navbar-brand" href="http://tutacare.github.io/tutaptr/">TutaPTR</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/') }}">{{trans('tutaptr.home')}}</a></li>
+                    <li><a href="{{ url('/') }}">{{trans('tuta.home')}}</a></li>
                     <li><a href="{{ url('/mybook') }}">My Book</a></li>
                 </ul>
 
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::guest())
-                        <li><a href="{{ url('/auth/login') }}">{{trans('tutaptr.login')}}</a></li>
-                        <li><a href="{{ url('/auth/register') }}">{{trans('tutaptr.register')}}</a></li>
+                        <li><a href="{{ url('/auth/login') }}">{{trans('tuta.login')}}</a></li>
+                        <li><a href="{{ url('/auth/register') }}">{{trans('tuta.register')}}</a></li>
                     @else
                         @if (Auth::user()->role == 'Administrator')
-                          <li><a href="{{ url('/dashboard/posts') }}">{{trans('tutaptr.posts')}}</a></li>
+                          <li><a href="{{ url('/dashboard/posts') }}">{{trans('tuta.posts')}}</a></li>
                         @endif
-                        <li><a href="{{ url('/deposit') }}">{{trans('tutaptr.balance')}} : {{ Auth::user()->balance }}</a></li>
+                        <li><a href="{{ url('/deposit') }}">{{trans('tuta.balance')}} : {{ Auth::user()->balance }}</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
@@ -55,7 +48,7 @@
                                   <li><a href="{{ url('/dashboard/users') }}">Users</a></li>
                                 @endif
                                 <li class="divider"></li>
-                                <li><a href="{{ url('/auth/logout') }}">{{trans('tutaptr.logout')}}</a></li>
+                                <li><a href="{{ url('/auth/logout') }}">{{trans('tuta.logout')}}</a></li>
                             </ul>
                         </li>
                     @endif
@@ -67,7 +60,7 @@
     @yield('content')
     <footer class="footer hidden-print">
       <div class="container">
-        <p class="text-muted">You are using <a href="http://tutacare.github.io/tutapos">TutaPTR</a> v0.1-alpha by <a href="https://twitter.com/tutacare">Irfan Mahfudz Guntur</a>.
+        <p class="text-muted">You are using <a href="http://tutacare.github.io/tutaptr">TutaPTR</a> v0.1-alpha by <a href="https://twitter.com/tutacare">Irfan Mahfudz Guntur</a>.
         </p>
       </div>
     </footer>
